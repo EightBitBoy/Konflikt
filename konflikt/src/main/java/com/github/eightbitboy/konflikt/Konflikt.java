4ee46e21c273afd6ee575be515cd58d39c46d255
EightBitBoy <haertel.dev@gmail.com>
2012-08-25 16:15:55 +0200

    add a simple application

--- konflikt/src/main/java/com/github/eightbitboy/konflikt/Konflikt.java
+++ konflikt/src/main/java/com/github/eightbitboy/konflikt/Konflikt.java
@@ -1,7 +1,14 @@
 package com.github.eightbitboy.konflikt;
 
-public class Konflikt{
-	public static void main(String args[]){
-		System.out.println("Hello!");
+import com.jme3.app.SimpleApplication;
+
+public class Konflikt extends SimpleApplication {
+	public static void main(String args[]) {
+		Konflikt application = new Konflikt();
+		application.start();
+	}
+
+	@Override
+	public void simpleInitApp() {
 	}
 }
